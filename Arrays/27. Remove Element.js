54b6653ec6638bd4e55a89ae9692e7a93a0a78f2
Nikhil26062000 <nikhilsbn06@gmail.com>
2026-07-02 21:03:48 +0530

    Add function to remove specified element from array

---
+++ Arrays/27. Remove Element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let temp=0;
+    for (let i=0;i<nums.length;i++){
+        if(nums[i]==val){
+            continue;
+        }else{
+            nums[temp] = nums[i];
+            temp++
+        }
+    }
+    return temp;
+};
